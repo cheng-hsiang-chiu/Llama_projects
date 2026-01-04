b962718da36087a55f5fd51037db1bbe60ae18ac
cheng-hsiang-chiu <58955345+cheng-hsiang-chiu@users.noreply.github.com>
2026-01-03 20:37:12 -0600

    Create Assigning_roles_to_Llama.py

---
+++ Assigning_roles_to_Llama.py
@@ -0,0 +1,35 @@
+# Import the Llama class
+from llama_cpp import Llama
+
+
+prompt = "Give me four short steps to troubleshoot my internet connection."
+
+conv = [
+	# Complete the user message
+	{
+        "role": "user",
+	    "content": prompt
+    }
+]
+
+# Pass the conversation to the model
+result = llm.create_chat_completion(messages=conv, max_tokens=20)
+print(result)
+
+
+
+# Add a system message to the conversation list
+conv = [
+	{
+        "role": "system",
+        "content": "You are a helpful and professional customer support assistant for an internet service provider. If the question or instruction doesn't relate to internet service, quote the response: 'Sorry, I can't answer that.'"},
+	{
+        "role": "user",
+	    "content": "Help me decide which stocks to invest in."
+    }
+]
+
+result = llm.create_chat_completion(messages=conv, max_tokens=15)
+# Extract the model response from the result object
+assistant_content = result["choices"][0]["message"]["content"]
+print(assistant_content)
